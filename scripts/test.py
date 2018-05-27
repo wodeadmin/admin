dfb20d63c7d2b335fbce59cafed9b2ff7d017067
wodeadmin <1393651724@qq.com>
2018-05-27 16:17:00 +0800

    描述

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,12 @@
-import pytest
+import pytest,allure
 
 
 class Test_001:
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3,4])
+    def test_abc(self,a):
+        assert a != 3
+    @allure.step(title="测试步骤002")
+    @pytest.mark.parametrize("a",[1,6,3,4])
     def test_abc(self,a):
         assert a != 3
